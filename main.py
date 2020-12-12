218d7095871036609fa4c537e38a8494e69bc256
Satyam <thesatyam10@gmail.com>
2020-12-12 14:36:30 +0530

    Improve some bugs in this project.

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ def displayBooks():
 
 # Issue the book from the library.
 def bookAvailable():
+    authorName = input("Enter Your full name: ")
     issueBooks = input("Enter the name of the book you want to issue: ")
     listOfBooks.remove(issueBooks)
+    with open("authorName.txt","a") as f1:  # write the name of the student who issue the book
+                f1.write(authorName+"\t"+issueBooks+"\n")
     print(f"{issueBooks} is issued in your name. So handle carefully and return within 30 days.")
 
 # Return the book to the library.
 def returnBook():
+    returnName = input("Enter your full name: ")
     returnBooks = input("Enter the name of the book you want to return: ")
     listOfBooks.append(returnBooks)
+    with open("returnBooks.txt",'a') as f2:# write the name of the student who return the book
+                f2.write(returnName+"\t"+returnBooks+"\n")
     print("Thanks for returning this book. I hope you enjoying reading books and Visit Again.")
     return returnBooks
 
@@ -33,15 +39,9 @@ try:
         a = int(input("Enter a choice: "))
         if a == 1:
             displayBooks()  # Display all books
-        elif a == 2:
-            with open("authorName.txt","a") as f1:  # write the name of the student who issue the book
-                authorName = input("Enter Your full name: ")
-                f1.write(authorName+"\n")
+        elif a == 2:            
             bookAvailable()
-        elif a == 3:
-            with open("returnBooks.txt",'a') as f2:# write the name of the student who return the book
-                returnName = input("Enter your full name: ")
-                f2.write(returnName+"\n")
+        elif a == 3:           
             returnBook()
         elif a == 4:
             print(f"Thank you {returnName} for visiting our library")
